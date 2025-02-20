7ec5854cb417de65b2885df3eb1670b5ff77e7ca
YoannSME <yoann.smets@univ-tlse3.fr>
2025-02-20 08:17:28 +0100

    fix

--- LesPirates/src/cartes/TypeCarte.java
+++ LesPirates/src/cartes/TypeCarte.java
@@ -1,17 +1,11 @@
 package cartes;
 
 public enum TypeCarte {
-	AbordageReussi("Abordage Reussi"), 
-	DiscoursInspirant("Discours Inspirant"),
-	MainDeFer("Main de fer"),
-	CoupDeSabre("Coup de Sabre"),
-	CarteVole("Carte vole"),
-	CarteRegen("Carte regen");
-	private String nomCarte;
-
-	private TypeCarte(String nomCarte) {
-		this.nomCarte = nomCarte;
-	}
-
-
+	AbordageReussi,
+	DiscoursInspirant,
+	MainDeFer,
+	CoupDeSabre,
+	VoleDeCarte,
+	RegenerationHP;
+	
 }
